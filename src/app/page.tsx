7913352d500a39ef9e21d6761105c34769486129
iigodpon <160293233+iigodpon@users.noreply.github.com>
2024-10-31 18:34:19 +0700

    ทำhead bar
    ทดลองโหลด font มา ใช้ไม่ได้ ค่อยว่ากันใหม่

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,18 @@ import Wallpaper from "../../public/pic/บ่อ/11.jpg";
 export default function Home() {
   return (
     <div className="bg-[#131314] font-[family-name:var(--font-geist-sans)]">
-      <div className="bg-[#131314] w-full h-[60px]"></div>
+      <div className="bg-[#131314] w-full h-[60px] flex items-center px-4">
+        <span className="text-white text-[30px]">วรเดช</span>
+        <span className="text-white pl-1">ฟาร์มทะเล</span>
+      </div>
 
-      <Image src={Wallpaper} alt="" className="w-full h-[300px] opacity-90" />
+      <div className="flex justify-center">
+        <Image
+          src={Wallpaper}
+          alt=""
+          className="w-full lg:w-[1300px] lg:h-[500px] h-[300px] opacity-90"
+        />
+      </div>
 
       <div className="w-full h-[1000px]"></div>
     </div>
