3b0bb3cfe362f9aaeb490fd62a8d866e43e0f476
iigodpon <160293233+iigodpon@users.noreply.github.com>
2024-10-31 17:38:45 +0700

    ปรับopacity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,12 @@ import Wallpaper from "../../public/pic/บ่อ/11.jpg";
 
 export default function Home() {
   return (
-    <div className="font-[family-name:var(--font-geist-sans)]">
+    <div className="bg-[#131314] font-[family-name:var(--font-geist-sans)]">
       <div className="bg-[#131314] w-full h-[60px]"></div>
-      <Image
-        src={Wallpaper}
-        alt=""
-        className="w-full h-[300px]"
-        objectFit="cover"
-      />
+
+      <Image src={Wallpaper} alt="" className="w-full h-[300px] opacity-90" />
+
+      <div className="w-full h-[1000px]"></div>
     </div>
   );
 }
